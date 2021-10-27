bd19002cebc3cfbaba8560f410c536c663c64d16
Luna <87862405+lunatsukiii@users.noreply.github.com>
2021-10-27 22:07:09 +0200

    start

---
+++ virtual_closet/android/app/src/main/kotlin/com/example/virtual_closet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.virtual_closet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ virtual_closet/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.purple[100],
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Card(
+                child: Image.asset(
+                  'assets/tshirt.jpg',
+                  height: 150,
+                ),
+              ),
+              Card(
+                child: Image.asset(
+                  'assets/pants.jpg',
+                  height: 150,
+                ),
+              ),
+              Card(
+                child: Image.asset(
+                  'assets/shoes.jpg',
+                  height: 150,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
