20183edd714dcecff7aed2aa6363c3131a16a8b6
Luna <87862405+lunatsukiii@users.noreply.github.com>
2021-10-28 14:39:06 +0200

    Update main.dart

--- virtual_closet/lib/main.dart
+++ virtual_closet/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
