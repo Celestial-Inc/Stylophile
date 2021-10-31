412cef90d610ce2f627248e0c591df8b8b783d08
stelladrc <stella.darcy@stud.edubs.ch>
2021-10-31 15:08:22 +0100

    refactor to remove duplication
    The build and buildImage methods were duplicated three times.
    We escalated the methods up to the top level scope, and removed the duplication.
    
    Co-Authored-By: Hamlet D'Arcy <186401+hamletdrc@users.noreply.github.com>

--- virtual_closet/lib/main.dart
+++ virtual_closet/lib/main.dart
@@ -3,6 +3,32 @@ import 'package:carousel_slider/carousel_slider.dart';
 
 main() => runApp(MyApp());
 
+Widget buildImageFromAsset(String urlImage1, int index) => Container(
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      color: Colors.pink[200],
+      child: Image.asset(
+        urlImage1,
+        fit: BoxFit.cover,
+      ),
+    );
+
+Widget buildCarousel(BuildContext context, imageList) => Scaffold(
+      backgroundColor: Colors.purple[100],
+      body: Center(
+        child: CarouselSlider.builder(
+          options: CarouselOptions(
+              height: 200, enlargeStrategy: CenterPageEnlargeStrategy.height),
+          itemCount: imageList.length,
+          itemBuilder: (context, index, realIndex) {
+            final urlImage1 = imageList[index];
+
+            return buildImageFromAsset(urlImage1, index);
+          },
+        ),
+      ),
+    );
+
+
 class MyApp extends StatelessWidget {
   //hello
   @override
@@ -13,9 +39,9 @@ class MyApp extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Container(child: Carousel1(), height: 200),
-            Container(child: Carousel2(), height: 200),
-            Container(child: Carousel3(), height: 200)
+            Container(child: ShirtCarousel(), height: 200),
+            Container(child: PantCarousel(), height: 200),
+            Container(child: ShoeCarousel(), height: 200)
           ],
           /* child: Column(
             mainAxisSize: MainAxisSize.max,
@@ -48,99 +74,33 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Carousel1 extends StatelessWidget {
-  final urlImages1 = [
+class ShirtCarousel extends StatelessWidget {
+  final imageList = [
     'assets/images/shirt01.jpg',
     'assets/images/shirt02.jpg',
     'assets/images/shirt03.jpg'
   ];
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.purple[100],
-        body: Center(
-          child: CarouselSlider.builder(
-            options: CarouselOptions(
-                height: 200, enlargeStrategy: CenterPageEnlargeStrategy.height),
-            itemCount: urlImages1.length,
-            itemBuilder: (context, index, realIndex) {
-              final urlImage1 = urlImages1[index];
-
-              return buildImage(urlImage1, index);
-            },
-          ),
-        ),
-      );
-  Widget buildImage(String urlImage1, int index) => Container(
-        margin: EdgeInsets.symmetric(horizontal: 8),
-        color: Colors.pink[200],
-        child: Image.asset(
-          urlImage1,
-          fit: BoxFit.cover,
-        ),
-      );
+  Widget build(BuildContext context) => buildCarousel(context, imageList);
 }
 
-class Carousel2 extends StatelessWidget {
-  final urlImages2 = [
+class PantCarousel extends StatelessWidget {
+  final imageList = [
     'assets/images/pants01.jpg',
     'assets/images/pants02.jpeg',
     'assets/images/pants03.jpg'
   ];
   @override
-  Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.purple[100],
-        body: Center(
-          child: CarouselSlider.builder(
-            options: CarouselOptions(
-                height: 200, enlargeStrategy: CenterPageEnlargeStrategy.height),
-            itemCount: urlImages2.length,
-            itemBuilder: (context, index, realIndex) {
-              final urlImage2 = urlImages2[index];
-
-              return buildImage(urlImage2, index);
-            },
-          ),
-        ),
-      );
-  Widget buildImage(String urlImage2, int index) => Container(
-        margin: EdgeInsets.symmetric(horizontal: 8),
-        color: Colors.pink[200],
-        child: Image.asset(
-          urlImage2,
-          fit: BoxFit.cover,
-        ),
-      );
+  Widget build(BuildContext context) => buildCarousel(context, imageList);
 }
 
-class Carousel3 extends StatelessWidget {
-  final urlImages3 = [
+class ShoeCarousel extends StatelessWidget {
+  final imageList = [
     'assets/images/shoes01.jpg',
     'assets/images/shoes02.png',
     'assets/images/shoes03.jpg'
   ];
   @override
-  Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.purple[100],
-        body: Center(
-          child: CarouselSlider.builder(
-            options: CarouselOptions(
-                height: 200, enlargeStrategy: CenterPageEnlargeStrategy.height),
-            itemCount: urlImages3.length,
-            itemBuilder: (context, index, realIndex) {
-              final urlImage3 = urlImages3[index];
-
-              return buildImage(urlImage3, index);
-            },
-          ),
-        ),
-      );
-  Widget buildImage(String urlImage3, int index) => Container(
-        margin: EdgeInsets.symmetric(horizontal: 8),
-        color: Colors.pink[200],
-        child: Image.asset(
-          urlImage3,
-          fit: BoxFit.cover,
-        ),
-      );
+  Widget build(BuildContext context) => buildCarousel(context, imageList);
 }
