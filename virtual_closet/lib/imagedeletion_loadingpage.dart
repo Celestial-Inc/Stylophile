1c469fedfdbbc05e5d4331bba72b67cb21e5030f
lunatsukiii <lunaatsukii@gmail.com>
2021-11-10 20:57:45 +0100

    does not send you to loading page after pressing yes
    line 160

---
+++ virtual_closet/lib/imagedeletion_loadingpage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LoadingImageDeletion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(body: Center(child: Text('loading...')));
+  }
+}
