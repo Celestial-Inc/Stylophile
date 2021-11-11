d219f6752e3770ca8cbde46a18018eb3decdaaf5
lunatsukiii <lunaatsukii@gmail.com>
2021-11-11 20:27:13 +0100

    refacture wardrobe page
    extract  method

--- virtual_closet/lib/wardrobe_page.dart
+++ virtual_closet/lib/wardrobe_page.dart
@@ -26,6 +26,45 @@ class _WardrobePageState extends State<WardrobePage> {
     );
   }
 
+  Widget buildDeleteButton(Box hiveBox, int index) {
+    return Positioned(
+        height: 20,
+        width: 20,
+        right: 0.0,
+        top: 0.0,
+        child: FloatingActionButton(
+          child: const Icon(
+            Icons.close_outlined,
+            size: 10,
+          ),
+          backgroundColor: Colors.purple[600],
+          onPressed: () => showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Warning!'),
+              content: const Text('Do you wish to delete this Image?'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => {Navigator.pop(context, 'No')},
+                  child: const Text('No'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, 'Yes');
+                    hiveBox.deleteAt(index);
+                    setState(() {
+                      // set forceRefresh to the opposite of whatever it is. It just flips between true and false.
+                      _forceRefresh = !_forceRefresh;
+                    });
+                  },
+                  child: const Text('Yes'),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
   Widget buildImageFromAsset(Box hiveBox, int index) {
     String imageAsString = hiveBox.getAt(index);
     var container = Container(
@@ -44,55 +83,22 @@ class _WardrobePageState extends State<WardrobePage> {
 
     // if there are no items then just display a nice text message
     if (itemCount == 0) {
-      return Text('Press Add to add ' + hiveBox.name.toString(), style: TextStyle(fontSize: 40));
+      return Text('Press Add to add ' + hiveBox.name.toString(),
+          style: TextStyle(fontSize: 40));
     }
 
     return Scaffold(
       backgroundColor: Colors.purple[100],
       body: Center(
         child: CarouselSlider.builder(
-          options: CarouselOptions(height: 200, enlargeStrategy: CenterPageEnlargeStrategy.height),
+          options: CarouselOptions(
+              height: 200, enlargeStrategy: CenterPageEnlargeStrategy.height),
           itemCount: itemCount,
           itemBuilder: (context, index, realIndex) {
             return Stack(
               children: [
                 buildImageFromAsset(hiveBox, index),
-                Positioned(
-                    height: 20,
-                    width: 20,
-                    right: 0.0,
-                    top: 0.0,
-                    child: FloatingActionButton(
-                      child: const Icon(
-                        Icons.close_outlined,
-                        size: 10,
-                      ),
-                      backgroundColor: Colors.purple[600],
-                      onPressed: () => showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: const Text('Warning!'),
-                          content: const Text('Do you wish to delete this Image?'),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () => {Navigator.pop(context, 'No')},
-                              child: const Text('No'),
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context, 'Yes');
-                                hiveBox.deleteAt(index);
-                                setState(() {
-                                  // set forceRefresh to the opposite of whatever it is. It just flips between true and false.
-                                  _forceRefresh = !_forceRefresh;
-                                });
-                              },
-                              child: const Text('Yes'),
-                            ),
-                          ],
-                        ),
-                      ),
-                    )),
+                buildDeleteButton(hiveBox, index),
               ],
             );
           },
