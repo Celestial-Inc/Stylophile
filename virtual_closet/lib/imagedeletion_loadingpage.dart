c4177c6486583dd088632e5a21b675d51379ba67
stelladrc <stella.darcy@stud.edubs.ch>
2021-11-11 10:26:18 +0100

    Simplify wardrobe page to just 1 class
    You need to refresh the page after you delete an item.
    For this to happen, you need just 1 class in the file.
    Otherwise, the carousel class doesn't know how to update
    the wardrobe page because it is a child widget and does
    not know about the parent widget.

--- virtual_closet/lib/imagedeletion_loadingpage.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class LoadingImageDeletion extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(body: Center(child: Text('loading...')));
-  }
-}
--- virtual_closet/lib/wardrobe_page.dart
+++ virtual_closet/lib/wardrobe_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:virtual_closet/imagedeletion_loadingpage.dart';
-import 'package:virtual_closet/loading_screen.dart';
 import 'package:hive/hive.dart';
 import 'dart:convert';
 
@@ -18,109 +16,76 @@ class _WardrobePageState extends State<WardrobePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(child: ShirtCarousel(), height: 200),
-          Container(child: PantCarousel(), height: 200),
-          Container(child: ShoeCarousel(), height: 200)
+          Container(child: buildCarousel(context, 'shirts'), height: 200),
+          Container(child: buildCarousel(context, 'bottoms'), height: 200),
+          Container(child: buildCarousel(context, 'shoes'), height: 200)
         ],
       ),
     );
   }
-}
 
-class ShirtCarousel extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Box box = Hive.box('shirts');
-    return buildCarousel(context, box);
+  Widget buildImageFromAsset(Box hiveBox, int index) {
+    String imageAsString = hiveBox.getAt(index);
+    var container = Container(
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      color: Colors.pink[200],
+      child: Image.memory(base64Decode(imageAsString), fit: BoxFit.cover),
+    );
+    return container;
   }
-}
 
-class PantCarousel extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Box box = Hive.box('bottoms');
-    return buildCarousel(context, box);
-  }
-}
+  Widget buildCarousel(BuildContext context, String boxName) {
+    // boxName is either 'shirts', 'bottoms', or 'shoes'
+    Box hiveBox = Hive.box(boxName);
 
-class ShoeCarousel extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Box box = Hive.box('shoes');
-    return buildCarousel(context, box);
-  }
-}
-
-Widget buildImageFromAsset(Box hiveBox, int index) {
-  String imageAsString = hiveBox.getAt(index);
-  var container = Container(
-    margin: EdgeInsets.symmetric(horizontal: 8),
-    color: Colors.pink[200],
-    child: Image.memory(base64Decode(imageAsString), fit: BoxFit.cover),
-  );
-  return container;
-}
-
-Widget buildCarousel(BuildContext context, hiveBox) {
-  var itemCount = hiveBox.length;
-  return Scaffold(
-    backgroundColor: Colors.purple[100],
-    body: Center(
-      child: CarouselSlider.builder(
-        options: CarouselOptions(height: 200, enlargeStrategy: CenterPageEnlargeStrategy.height),
-        itemCount: itemCount,
-        itemBuilder: (context, index, realIndex) {
-          return Stack(
-            children: [
-              buildImageFromAsset(hiveBox, index),
-              Positioned(
-                  height: 20,
-                  width: 20,
-                  right: 0.0,
-                  top: 0.0,
-                  child: FloatingActionButton(
-                    child: const Icon(
-                      Icons.close_outlined,
-                      size: 10,
-                    ),
-                    backgroundColor: Colors.purple[600],
-                    onPressed: () => showDialog<String>(
-                      context: context,
-                      builder: (BuildContext context) => AlertDialog(
-                        title: const Text('Warning!'),
-                        content: const Text('Do you wish to delete this Image?'),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () => {Navigator.pop(context, 'No')},
-                            child: const Text('No'),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context, 'Yes');
-                              hiveBox.deleteAt(
-                                  index); //TODO: only works if you switch to a different page after deletion and then come back, -> maybe go to loading page and back, this way means no stateful widget = less work same result?
-                              hiveBox.loadHiveImages();
-                              Navigator.push(
-                                context,
-<<<<<<< HEAD
-                                MaterialPageRoute(builder: (context) => LoadingScreen()),
-=======
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        LoadingImageDeletion()),
->>>>>>> 53d7c8f (does not send you to loading page after pressing yes)
-                              );
-                            },
-                            child: const Text('Yes'),
-                          ),
-                        ],
+    var itemCount = hiveBox.length;
+    return Scaffold(
+      backgroundColor: Colors.purple[100],
+      body: Center(
+        child: CarouselSlider.builder(
+          options: CarouselOptions(height: 200, enlargeStrategy: CenterPageEnlargeStrategy.height),
+          itemCount: itemCount,
+          itemBuilder: (context, index, realIndex) {
+            return Stack(
+              children: [
+                buildImageFromAsset(hiveBox, index),
+                Positioned(
+                    height: 20,
+                    width: 20,
+                    right: 0.0,
+                    top: 0.0,
+                    child: FloatingActionButton(
+                      child: const Icon(
+                        Icons.close_outlined,
+                        size: 10,
                       ),
-                    ),
-                  )),
-            ],
-          );
-        },
+                      backgroundColor: Colors.purple[600],
+                      onPressed: () => showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: const Text('Warning!'),
+                          content: const Text('Do you wish to delete this Image?'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => {Navigator.pop(context, 'No')},
+                              child: const Text('No'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context, 'Yes');
+                                hiveBox.deleteAt(index);
+                              },
+                              child: const Text('Yes'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )),
+              ],
+            );
+          },
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
