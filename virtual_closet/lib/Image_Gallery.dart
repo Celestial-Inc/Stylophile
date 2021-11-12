54ec6399de5332994466ba0a662a86f278d42307
Luna <lunaatsukii@gmail.com>
2021-11-12 13:19:12 +0100

    Update Image_Gallery.dart

--- virtual_closet/lib/Image_Gallery.dart
+++ virtual_closet/lib/Image_Gallery.dart
@@ -1,12 +1,27 @@
-import 'package:flutter/material.dart';
 
-class ImageGallery extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+class ImageGallery extends StatefulWidget {
+  @override
+  _ImageGalleryState createState() => _ImageGalleryState();
+}
+
+class _ImageGalleryState extends State<ImageGallery> {
+  Widget buildImage(BuildContext context) {
+    List<Widget> widgets = [];
+    var hiveBox = Hive.box('shirts');
+    var length = hiveBox.length;
+    for (int x = 0; x < length; x++) {
+      var hiveImage = hiveBox.getAt(x);
+      widgets.add(buildImage(hiveImage));
+    return
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Images'),
-      ),
-    );
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
