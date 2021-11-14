b765dd35f6c0f1fe2a01655151ccc77a6cf09f1a
lunatsukiii <lunaatsukii@gmail.com>
2021-11-14 22:02:57 +0100

    image gallery update
    appbar goes away while scrolling

--- virtual_closet/lib/Image_Gallery.dart
+++ virtual_closet/lib/Image_Gallery.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:hive/hive.dart';
 
 class ImageGallery extends StatefulWidget {
@@ -18,9 +19,77 @@ class _ImageGalleryState extends State<ImageGallery> {
     return widgets;
   }
 
+  late ScrollController _scrollViewController;
+  bool _showAppbar = true;
+  bool isScrollingDown = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollViewController = new ScrollController();
+    _scrollViewController.addListener(() {
+      if (_scrollViewController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        if (!isScrollingDown) {
+          isScrollingDown = true;
+          _showAppbar = false;
+          setState(() {});
+        }
+      }
+
+      if (_scrollViewController.position.userScrollDirection ==
+          ScrollDirection.forward) {
+        if (isScrollingDown) {
+          isScrollingDown = false;
+          _showAppbar = true;
+          setState(() {});
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollViewController.dispose();
+    _scrollViewController.removeListener(() {});
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            AnimatedContainer(
+              height: _showAppbar ? 56.0 : 0.0,
+              duration: Duration(milliseconds: 200),
+              child: AppBar(
+                backgroundColor: Color.fromARGB(250, 230, 150, 200),
+                centerTitle: true,
+                title: Text('Images'),
+                actions: <Widget>[
+                  //add buttons here
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                controller: _scrollViewController,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        child: Text(
+                      'Images will be displayed here, check out Website: https://daily-dev-tips.com/posts/building-a-photo-grid-view-in-flutter/',
+                      style: TextStyle(fontSize: 50),
+                    )),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
