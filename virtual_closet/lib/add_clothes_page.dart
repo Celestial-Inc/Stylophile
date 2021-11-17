88ba3b71d5814bdf14d1cb5e23104b415216d8fe
stelladrc <stella.darcy@stud.edubs.ch>
2021-11-17 23:11:33 +0100

    Added cropper!!! (First Draft)
    Needs improvements:
    Understand Code
    Be able to move image being cropped
    Remove 'Crop Demo' Bar at the top

--- virtual_closet/lib/add_clothes_page.dart
+++ virtual_closet/lib/add_clothes_page.dart
@@ -1,10 +1,12 @@
-import 'dart:math';
-
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'dart:convert';
-import 'package:hive/hive.dart';
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:crop/crop.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddClothesPage extends StatefulWidget {
   @override
@@ -12,39 +14,54 @@ class AddClothesPage extends StatefulWidget {
 }
 
 class _AddClothesPageState extends State<AddClothesPage> {
-  bool isImageSelected = false;
-  String? base64Image;
+  final controller = CropController(aspectRatio: 3 / 4);
+  String? uncroppedImage;
   String? clothingType;
+  String? croppedImage;
 
-  void pickImage() async {
+  @override
+  void initState() {
+    super.initState();
+    _pickImage();
+  }
+
+  void _pickImage() async {
     final ImagePicker _picker = ImagePicker();
     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
     List<int> imageBytes = await image!.readAsBytes();
-    base64Image = base64Encode(imageBytes);
     setState(() {
-      isImageSelected = true;
+      uncroppedImage = base64Encode(imageBytes);
     });
   }
 
-  void pickClothing(String? value) async {
-    Random random = Random();
-    int randomNumber = random.nextInt(1000000);
-    Box shirtsBox = Hive.box(value!);
-    shirtsBox.put('myNewShirt' + randomNumber.toString(), base64Image);
+  void _pickClothing(String? value) async {
     setState(() {
       clothingType = value;
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    pickImage();
+  void _cropImage() async {
+    final pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    ui.Image cropped = await controller.crop(pixelRatio: pixelRatio);
+
+    ByteData? data = await cropped.toByteData(format: ui.ImageByteFormat.png);
+    if (data != null) {
+      String newCroppedImage = base64Encode(Uint8List.view(data!.buffer));
+
+      Random random = Random();
+      int randomNumber = random.nextInt(1000000);
+      Box hiveBox = Hive.box(clothingType!);
+      hiveBox.put('myNewClothing' + randomNumber.toString(), newCroppedImage);
+
+      setState(() {
+        croppedImage = newCroppedImage;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    if (isImageSelected == false) {
+    if (uncroppedImage == null) {
       return Scaffold(body: Center(child: Text('Loading...')));
     }
 
@@ -54,40 +71,72 @@ class _AddClothesPageState extends State<AddClothesPage> {
         children: <Widget>[
           ListTile(
             title: const Text('Top'),
-            leading: Radio<String>(
-                value: 'shirts',
-                groupValue: clothingType,
-                onChanged: pickClothing),
+            leading: Radio<String>(value: 'shirts', groupValue: clothingType, onChanged: _pickClothing),
           ),
           ListTile(
             title: const Text('Bottom'),
-            leading: Radio<String>(
-                value: 'bottoms',
-                groupValue: clothingType,
-                onChanged: pickClothing),
+            leading: Radio<String>(value: 'bottoms', groupValue: clothingType, onChanged: _pickClothing),
           ),
           ListTile(
             title: const Text('Shoe'),
-            leading: Radio<String>(
-                value: 'shoes',
-                groupValue: clothingType,
-                onChanged: pickClothing),
+            leading: Radio<String>(value: 'shoes', groupValue: clothingType, onChanged: _pickClothing),
           ),
         ],
+      );
+    }
+
+    if (croppedImage == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Crop Demo'),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              onPressed: _cropImage,
+              tooltip: 'Crop',
+              icon: const Icon(Icons.crop),
+            )
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                color: Colors.black,
+                padding: const EdgeInsets.all(8),
+                child: Crop(
+                  controller: controller,
+                  shape: BoxShape.rectangle,
+                  child: Image.memory(
+                    base64Decode(uncroppedImage!),
+                    fit: BoxFit.cover,
+                  ),
+                  /* It's very important to set `fit: BoxFit.cover`.
+                   Do NOT remove this line.
+                   There are a lot of issues on github repo by people who remove this line and their image is not shown correctly.
+                */
+                  helper: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       );
     }
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Container(
-            child: Text('Thank you, you added this picture:',
-                style: GoogleFonts.notoSans(fontSize: 40))),
+        Container(child: Text('Thank you, you added this picture:', style: GoogleFonts.notoSans(fontSize: 40))),
         Container(
           width: 500,
           height: 500,
           child: Image.memory(
-            base64Decode(base64Image!),
+            base64Decode(croppedImage!),
             fit: BoxFit.contain,
           ),
         ),
